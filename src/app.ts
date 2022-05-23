94d377114be20b8121a027b9ca9af32c04d8740d
rafaeelnunes <rafaelnfsq@gmail.com>
2022-05-23 11:27:09 -0300

    fix: cors policy

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,14 @@ import "express-async-errors";
 import router from "./routers/index.js";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
 
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
 const app = express();
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(json());
 app.use(router);
